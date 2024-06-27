97a1566334f696761eece81b32824511d0f1335c
vanstrouble <vanstrouble@proton.me>
2024-06-26 21:25:28 -0600

    Improve console accuracy messages Change GridSearchCV to RandomizedSearchCV

--- src/svm.py
+++ src/svm.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+from sklearn.datasets import load_iris, make_blobs
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
 
 class SVM:
     def __init__(self, learning_rate=0.001, lambda_param=0.1, n_iters=1000) -> None:
@@ -35,7 +40,7 @@ class SVM:
         return {
             "learning_rate": self.lr,
             "lambda_param": self.lambda_param,
-            "n_iters": self.n_iters
+            "n_iters": self.n_iters,
         }
 
     def set_params(self, **params):
@@ -45,11 +50,6 @@ class SVM:
 
 
 if __name__ == "__main__":
-    from sklearn.datasets import load_iris, make_blobs
-    from sklearn.model_selection import train_test_split, GridSearchCV
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.metrics import classification_report, accuracy_score
-
     X, y = load_iris(return_X_y=True)
     X = X[:100, [0, 2]]
     y = y[:100]
@@ -68,10 +68,12 @@ if __name__ == "__main__":
         "n_iters": [100, 1000, 5000, 10000],
     }
 
-    grid_search = GridSearchCV(SVM(), param_grid, cv=5, scoring="accuracy", verbose=1, n_jobs=-1)
+    grid_search = RandomizedSearchCV(
+        SVM(), param_grid, cv=3, scoring="accuracy", verbose=1, n_jobs=-1
+    )
     grid_search.fit(X_train, y_train)
-    print(grid_search.best_params_)
-    print(grid_search.best_score_)
+    print(f"Best parameters: \n{grid_search.best_params_}")
+    print(f"\nTrain Accuracy: {grid_search.best_score_}")
 
     best_svm, best_params = grid_search.best_estimator_, grid_search.best_params_
     y_pred = best_svm.predict(X_test)
@@ -80,7 +82,7 @@ if __name__ == "__main__":
     # svm.fit(X_train, y_train)
 
     # y_pred = svm.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
+    print(f"Model Accuracy: {accuracy_score(y_test, y_pred)}")
 
     # X, y = make_blobs(
     #     n_samples=100, n_features=2, centers=2, cluster_std=1.05, random_state=123
